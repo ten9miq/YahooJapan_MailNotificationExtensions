0443cfef412a6b141b3a88e7ffb35472f3b1dceb
53JIlLenWe11 <ruin.of.messger@gmail.com>
2019-10-12 21:07:55 +0900

    とりあえず、eslint+prettierで整形

--- background.js
+++ background.js
@@ -32,20 +32,22 @@ async function set_eappid() {
 		await connecter.get_method();
 		const text = await connecter.get_text();
 
-		const patterns = [
-		    /eappid\u0022:\u0022(.+?)\u0022,\u0022/,
-		    /eappid\\u0022:\\u0022(.+?)\\u0022,\\u0022/,
-		];
+		const patterns = [/eappid\u0022:\u0022(.+?)\u0022,\u0022/, /eappid\\u0022:\\u0022(.+?)\\u0022,\\u0022/];
 		let page_eappid = null;
-		for ( let pattern of patterns ) {
+		for (let pattern of patterns) {
 			page_eappid = text.match(pattern);
-			if ( page_eappid !== null ) {
+			if (page_eappid !== null) {
 				break;
 			}
 		}
 		console.log('取得したeappid', page_eappid);
 		if (page_eappid !== null) {
-			chrome.storage.local.set({ eappid: page_eappid[1] }, () => {});
+			chrome.storage.local.set(
+				{
+					eappid: page_eappid[1],
+				},
+				() => {}
+			);
 			return page_eappid[1];
 		}
 	} catch (error) {
@@ -61,7 +63,12 @@ async function get_eappid() {
 }
 
 function clear_eappid() {
-	chrome.storage.local.set({ eappid: null }, () => {});
+	chrome.storage.local.set(
+		{
+			eappid: null,
+		},
+		() => {}
+	);
 }
 
 async function is_login() {
@@ -78,8 +85,8 @@ async function is_login() {
 	// - ID新規作成リンク: https://account.edit.yahoo.co.jp/registration?...
 	// - 登録情報: https://login.yahoo.co.jp/config/login?...
 	const a_in_login_box = html.querySelectorAll('#Login a');
-	for ( a of a_in_login_box ) {
-		if ( a.href.match(/^https:\/\/accounts.yahoo.co.jp\/profile\?/) ) {
+	for (a of a_in_login_box) {
+		if (a.href.match(/^https:\/\/accounts.yahoo.co.jp\/profile\?/)) {
 			return true;
 		}
 	}
@@ -99,7 +106,7 @@ async function get_mail_count_core(eappid) {
 async function get_mail_count() {
 	try {
 		const eappid = await get_eappid();
-		if ( eappid  ) {
+		if (eappid) {
 			try {
 				return await get_mail_count_core(eappid);
 			} catch (error) {
@@ -123,7 +130,7 @@ async function get_mail_count2() {
 			return 'login';
 		}
 		const eappid = await set_eappid();
-		if ( ! eappid ) {
+		if (!eappid) {
 			return 0;
 		}
 		return await get_mail_count_core(eappid);
